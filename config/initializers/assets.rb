d4702b90b9ce3820551ca6772053746741e5bc14
jzhang <zzhang@pasonatquila.com>
2016-06-24 19:11:27 +0900

    check header

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,13 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( classie.js )
+Rails.application.config.assets.precompile += %w( cbpAnimatedHeader.js )
+Rails.application.config.assets.precompile += %w( jqBootstrapValidation.js )
+Rails.application.config.assets.precompile += %w( contact_me.js )
+Rails.application.config.assets.precompile += %w( agency.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
